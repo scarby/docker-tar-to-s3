49f2b2b5ed433f2a47b8f36587dc080266554e72
Adam Scarborough <scarby2@gmail.com>
2017-03-22 23:28:18 +0000

    add gzip compression

--- files/tar-to-s3.sh
+++ files/tar-to-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
 set -e
-name="${product}-$(date '+%y-%m-%d').tar"
-tar -cvf ${name} /data
+name="${product}-$(date '+%y-%m-%d').tar.gz"
+tar -czvf ${name} /data
 s3cmd put ${name} s3://${bucket}/${name}
